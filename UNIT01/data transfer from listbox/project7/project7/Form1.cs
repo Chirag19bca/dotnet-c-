458482bb0bfc8983845cfd71fdbb30e21b795b64
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2024-02-22 10:06:58 +0530

    Add files via upload

---
+++ UNIT01/data transfer from listbox/project7/project7/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i,n;
+            n = listBox1.SelectedItems.Count;
+            for (i = 0; i <= n; i++)
+            {
+                listBox2.Items.Add(listBox1.Items[0]);
+                listBox1.Items.Remove(listBox1.Items[0]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(listBox2.SelectedItem);
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+    }
+}
