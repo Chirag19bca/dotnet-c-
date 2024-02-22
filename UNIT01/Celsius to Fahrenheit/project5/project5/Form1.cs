178aa66f34d821de042c600ed2b951e503b367b3
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2024-02-22 10:02:23 +0530

    Add files via upload

---
+++ UNIT01/Celsius to Fahrenheit/project5/project5/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float f, c;
+            if (radioButton1.Checked == true)
+            {
+                f = Convert.ToSingle(textBox1.Text);
+                c=(5*(f-32))/9;
+                MessageBox.Show(Convert.ToString(f)+" Fahrenheit is Equal to "+Convert.ToString(c)+" Celsius");
+            }
+            else if(radioButton2.Checked==true)
+            {
+                c = Convert.ToSingle(textBox1.Text);
+                f = ((9*c)/5) + 32;
+                MessageBox.Show(Convert.ToString(c) + " Celsisus is Equal to " + Convert.ToString(f) + " Fahrenheit");
+            }
+        }
+    }
+}
