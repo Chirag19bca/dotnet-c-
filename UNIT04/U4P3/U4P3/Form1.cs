2c7694a870f0e1abdc00b96a800c4b2edc090e5a
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2024-02-27 15:16:02 +0530

    Add files via upload

---
+++ UNIT04/U4P3/U4P3/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace U4P3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\aaa\Documents\mybca.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter adp = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\aaa\Documents\mybca.mdf;Integrated Security=True;Connect Timeout=30");
+            cmd = new SqlCommand("select * from tblbca",con);
+            adp = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+            adp.Fill(ds);
+            comboBox1.DisplayMember="username";
+            comboBox1.ValueMember = "id";
+            comboBox1.DataSource =ds.Tables[0];
+        }
+    }
+}
