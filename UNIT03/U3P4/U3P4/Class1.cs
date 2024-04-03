26222f2442789e6a0583f12dfdbc8038085248bb
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2024-04-03 12:39:07 +0530

    Add files via upload

---
+++ UNIT03/U3P4/U3P4/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3P4
+{
+    class book
+    {
+        int bookid;
+        string title;
+        long price;
+        public void getdata(int id)
+        {
+            bookid= id;
+        }
+        public void getdata(int id,string bname)
+        {
+            bookid = id;
+            title = bname;
+        }
+        public void getdata(int id, string bname,long bprice)
+        {
+            bookid = id;
+            title = bname;
+            price = bprice;
+        }
+    }
+}
